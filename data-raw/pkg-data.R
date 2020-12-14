310eb8e8dc9560f048b1d4f1f5b6e8e04410e345
josiahparry <josiah.parry@gmail.com>
2020-12-14 10:37:06 -0500

    add data

---
+++ data-raw/pkg-data.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+acs_edu <- read_csv("data-raw/acs_edu.csv")
+
+ecometrics <- read_csv("data-raw/ecometrics.csv") %>%
+  mutate(across(where(is.character), tolower))
+
+commute <- read_csv("data-raw/gba_commute.csv")
+
+suffolk_county <- sf::read_sf("data-raw/suffolk_acs.geojson")
+
+
+airbnb_hosts <- read_csv("data-raw/airbnb/hosts.csv", na = c("N/A"))
+airbnb_listings <- read_csv("data-raw/airbnb/listings.csv")
+airbnb_locations <- read_csv("data-raw/airbnb/locations.csv")
+airbnb_reviews <- read_csv("data-raw/airbnb/reviews.csv")
+
+
+usethis::use_data(acs_edu, ecometrics, commute, suffolk_county,
+                  airbnb_hosts, airbnb_listings, airbnb_locations, overwrite = TRUE)
