a6ffe25728ab240b445fc5e2e69ed062f6fa5b67
josiahparry <josiah.parry@gmail.com>
2021-01-08 10:40:10 -0500

    add datasets

--- data-raw/pkg-data.R
+++ data-raw/pkg-data.R
@@ -1,6 +1,11 @@
 library(tidyverse)
 
+
+acs_raw <- read_csv("data-raw/ACS_1317_TRACT.csv")
+
 acs_edu <- read_csv("data-raw/acs_edu.csv")
+
+acs_housing <- read_csv("data-raw/acs-housing.csv")
 
 ecometrics <- read_csv("data-raw/ecometrics.csv") %>%
   mutate(across(where(is.character), tolower))
@@ -16,5 +21,5 @@ airbnb_locations <- read_csv("data-raw/airbnb/locations.csv")
 airbnb_reviews <- read_csv("data-raw/airbnb/reviews.csv")
 
 
-usethis::use_data(acs_edu, ecometrics, commute, suffolk_county,
+usethis::use_data(acs_raw, acs_housing, acs_edu, ecometrics, commute, suffolk_county,
                   airbnb_hosts, airbnb_listings, airbnb_locations, overwrite = TRUE)
