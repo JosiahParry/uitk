a501b6f50d8ed6b8095eeff36bbdc479301b9943
josiahparry <josiah.parry@gmail.com>
2021-02-04 19:43:32 -0500

    add big belly raw

--- data-raw/pkg-data.R
+++ data-raw/pkg-data.R
@@ -14,12 +14,15 @@ commute <- read_csv("data-raw/gba_commute.csv")
 
 suffolk_county <- sf::read_sf("data-raw/suffolk_acs.geojson")
 
+big_belly <- read_csv("data-raw/downtown-big-belly.csv")
+big_belly_raw <- read_csv("data-raw/big-belly-2014.csv")
 
+airbnb <- read_csv("data-raw/airbnb/airbnb.csv")
 airbnb_hosts <- read_csv("data-raw/airbnb/hosts.csv", na = c("N/A"))
 airbnb_listings <- read_csv("data-raw/airbnb/listings.csv")
 airbnb_locations <- read_csv("data-raw/airbnb/locations.csv")
 airbnb_reviews <- read_csv("data-raw/airbnb/reviews.csv")
 
 
-usethis::use_data(acs_raw, acs_housing, acs_edu, ecometrics, commute, suffolk_county,
-                  airbnb_hosts, airbnb_listings, airbnb_reviews, airbnb_locations, overwrite = TRUE)
+usethis::use_data(acs_raw, acs_housing, acs_edu, ecometrics, commute, suffolk_county, big_belly, big_belly_raw,
+                  airbnb, airbnb_hosts, airbnb_listings, airbnb_reviews, airbnb_locations, overwrite = TRUE)
